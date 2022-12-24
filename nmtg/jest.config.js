05e762848fb696566813583a6772a491e38004dc
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-12-24 19:06:31 +0000

    clean up

--- nmtg/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // It suggests that a map from ordinary regex to module names that permit to stub out assets with a single module
-       moduleNameMapper: {
-          "@root(.*)$": "<rootDir>/src/$1"
-    },
-  }
