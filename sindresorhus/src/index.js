cd61f53a34a84d33890d1e14567297d61e6ced6a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-12-25 00:23:22 +0000

    main release

--- sindresorhus/src/index.js
+++ sindresorhus/src/index.js
@@ -1,14 +1,13 @@
-// import buttonComponent from './components/button';
 import buttonComponent from './components/button';
 
-// import divComponent from './components/div';
+import divComponent from './components/div';
 
 
 import heading from './components/heading-nmtg';
 
 import headingComponent from './components/heading';
 
-// import headingOneComponent from './components/headingOne';
+import headingOneComponent from './components/headingOne';
 
 import imageComponent from './components/image';
 
@@ -22,68 +21,44 @@ import linkComponent from './components/link';
 import listComponent from './components/list';
 import listItemComponent from './components/listItem';
 
-// button.js
+import paragraphComponent from './components/paragraph';
 
-// div.js
+import strongComponent from './components/strong';
 
-// heading-nmtg.js
-
-// heading.js
-
-// headingOne.js
-
-// image.js
-
-// imageLink.js
-
-// italic.js
-
-// link.js
-
-// list.js
-
-// listItem.js
-
-// paragraph.js
-
-// strong.js
-
-
-
-
-
-
-// import strong from './components/strong';
-// import { renderSubHeading, spanHeadingBlock } from './components/subheading';
-//--------
-
-
-// import paragraphComponent from './components/paragraph';
-
-// import separatorComponent from './components/separator';
-
-// import {
-//     imageComponent,
-//     imgWrapper
-// } from './components/image';
-
-// import buttonComponent2 from '../src/components/button2';
 
 const config = {id: '12', href: 'google.com'};
 console.log(buttonComponent(config))
 
-export { 
-    // buttonComponent, 
-    buttonComponent,
-    heading,  
-    //-----
-    // strong, 
-    // renderSubHeading,
-    // spanHeadingBlock,
-    //------
-    // imageComponent,
-    // imgWrapper,
-    // paragraphComponent,
-    linkComponent,
-    // separatorComponent
-};
+
+export {
+    buttonComponent, 
+    divComponent, 
+    heading, 
+    headingComponent, 
+    headingOneComponent, 
+    imageComponent,  
+    imageLink,  
+    italicComponent,  
+    linkComponent,  
+    listComponent,  
+    listItemComponent,  
+    paragraphComponent,  
+    strongComponent, 
+}
+
+
+// export { 
+//     // buttonComponent, 
+//     buttonComponent,
+//     heading,  
+//     //-----
+//     // strong, 
+//     // renderSubHeading,
+//     // spanHeadingBlock,
+//     //------
+//     // imageComponent,
+//     // imgWrapper,
+//     // paragraphComponent,
+//     linkComponent,
+//     // separatorComponent
+// };
