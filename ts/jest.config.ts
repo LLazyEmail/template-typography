fcb9326185ab83f9959be3868760a2131ae8b723
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-12-30 11:01:13 +0000

    132

--- ts/jest.config.ts
+++ ts/jest.config.ts
@@ -21,3 +21,13 @@ export default config;
 //     preset: "ts-jest",
 //     testEnvironment: "node"
 //   };
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// module.exports = {
+//   preset: "ts-jest",
+//   testEnvironment: "node",
+//   roots: ["<rootDir>/src"],
+//   // the following line is needed in order to grab modules from the
+//   // src folder without the need to write them relatively
+//   moduleDirectories: ["node_modules", "src"],
+// };
